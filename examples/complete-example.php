debd7235d1e4a89590141de724c3b3d4d40af914
CBe <github@claus-beckenbauer.de>
2017-01-23 13:00:29 +0100

    Example for poll and cancel

--- examples/complete-example.php
+++ examples/complete-example.php
@@ -8,6 +8,8 @@ use LeonardoTeixeira\Pushover\Client;
 use LeonardoTeixeira\Pushover\Message;
 use LeonardoTeixeira\Pushover\Priority;
 use LeonardoTeixeira\Pushover\Sound;
+use LeonardoTeixeira\Pushover\Receipt;
+use LeonardoTeixeira\Pushover\Status;
 use LeonardoTeixeira\Pushover\Exceptions\PushoverException;
 
 $client = new Client('YOUR_USER_CODE_HERE', 'YOUR_TOKEN_HERE');
@@ -23,8 +25,14 @@ $message->setHtml(true);
 $message->setDate(new \DateTime());
 
 try {
-    $client->push($message);
+    $receipt = $client->push($message);
     echo 'The message has been pushed!', PHP_EOL;
+    
+    $status = $client->poll($receipt);
+    echo 'Receipt has been polled!', PHP_EOL;
+    
+    $client->cancel($receipt);
+    echo 'Notification has been cancelled!', PHP_EOL;
 } catch (PushoverException $e) {
     echo 'ERROR: ', $e->getMessage(), PHP_EOL;
 }
